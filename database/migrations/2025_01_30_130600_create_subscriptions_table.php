551011d9cb1abd0e59e82209bd71991dabcc2c94
Sheshank singh <sheshank.singh@neoleaf.in>
2025-01-31 19:17:58 +0530

    first comment room meeting

---
+++ database/migrations/2025_01_30_130600_create_subscriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSubscriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('plan_name');
+            $table->integer('max_bookings_per_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('subscriptions');
+    }
+}
